533b4b0610326f0a0400f56c5d48a4f5f3b3ee09
kj.santos <kj.santos@hotmail.com>
2016-06-09 21:42:43 -0300

    Inicial

---
+++ vannsmobileService/Models/Map/ClientMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vannsmobileService.DataObjects;
+
+namespace vannsmobileService.Models.Map
+{
+    public class ClientMap : EntityTypeConfiguration<Client>
+    {
+        public ClientMap()
+        {
+            ToTable("Client");
+        }
+    }
+}
---
+++ vannsmobileService/Models/Map/ZoneMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vannsmobileService.DataObjects;
+
+namespace vannsmobileService.Models.Map
+{
+    public class ZoneMap : EntityTypeConfiguration<Zone>
+    {
+        public ZoneMap()
+        {
+            ToTable("Zone");
+        }
+    }
+}
